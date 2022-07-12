f9bed22c3b29ae88e2401cf449d16f1f3a4fd345
Nguyễn Sang <sang.nguyen@spiritlabs.co>
2022-07-12 20:04:10 +0700

    feat: update new dot file

--- .config/nvim/lua/core/lualine.lua
+++ .config/nvim/lua/core/lualine.lua
@@ -13,7 +13,6 @@ local diagnostics = {
 	sections = { "error", "warn" },
 	symbols = { error = " ", warn = " " },
 	colored = false,
-	update_in_insert = false,
 	always_visible = true,
 }
 
@@ -24,23 +23,9 @@ local diff = {
 	cond = hide_in_width,
 }
 
-local mode = {
-	"mode",
-	fmt = function(str)
-		return "-- " .. str .. " --"
-	end,
-}
-
 local filetype = {
 	"filetype",
 	icons_enabled = false,
-	icon = nil,
-}
-
-local branch = {
-	"branch",
-	icons_enabled = true,
-	icon = "",
 }
 
 local location = {
@@ -48,50 +33,26 @@ local location = {
 	padding = 0,
 }
 
--- cool function for progress
-local progress = function()
-	local current_line = vim.fn.line(".")
-	local total_lines = vim.fn.line("$")
-	local chars = { "__", "▁▁", "▂▂", "▃▃", "▄▄", "▅▅", "▆▆", "▇▇", "██" }
-	local line_ratio = current_line / total_lines
-	local index = math.ceil(line_ratio * #chars)
-	return chars[index]
-end
-
 local spaces = function()
 	return "spaces: " .. vim.api.nvim_buf_get_option(0, "shiftwidth")
 end
 
--- Custom lualine theme
-local custom_theme = require("lualine.themes.onedark")
-custom_theme.normal.c.bg = "none"
-
 lualine.setup({
 	options = {
+		globalstatus = true,
 		icons_enabled = true,
-		theme = custom_theme,
+		theme = "auto",
 		component_separators = { left = "", right = "" },
 		section_separators = { left = "", right = "" },
-		disabled_filetypes = { "alpha", "dashboard", "NvimTree", "Outline" },
+		disabled_filetypes = { "alpha", "dashboard" },
 		always_divide_middle = true,
 	},
 	sections = {
-		lualine_a = { branch, diagnostics },
-		lualine_b = { mode },
-		lualine_c = {},
-		-- lualine_x = { "encoding", "fileformat", "filetype" },
+		lualine_a = { "mode" },
+		lualine_b = { "branch" },
+		lualine_c = { diagnostics },
 		lualine_x = { diff, spaces, "encoding", filetype },
 		lualine_y = { location },
-		lualine_z = { progress },
+		lualine_z = { "progress" },
 	},
-	inactive_sections = {
-		lualine_a = {},
-		lualine_b = {},
-		lualine_c = { "filename" },
-		lualine_x = { "location" },
-		lualine_y = {},
-		lualine_z = {},
-	},
-	tabline = {},
-	extensions = {},
 })
