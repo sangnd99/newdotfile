99b5d1d628533164326ca9cfea4905e0475076c8
Sang Nguyen <sang.nguyen@navigosgroup.com>
2023-06-28 10:15:43 +0700

    fixed VNW-23491 add checkbox agree term and policy

--- .config/nvim/lua/lsp/lsp-handlers.lua
+++ .config/nvim/lua/lsp/lsp-handlers.lua
@@ -2,7 +2,7 @@ local M = {}
 
 local status_cmp_ok, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
 if not status_cmp_ok then
-	return
+  return
 end
 
 M.capabilities = vim.lsp.protocol.make_client_capabilities()
@@ -10,88 +10,99 @@ M.capabilities.textDocument.completion.completionItem.snippetSupport = true
 M.capabilities = cmp_nvim_lsp.default_capabilities(M.capabilities)
 
 M.setup = function()
-	local signs = {
+  local signs = {
 
-		{ name = "DiagnosticSignError", text = "" },
-		{ name = "DiagnosticSignWarn", text = "" },
-		{ name = "DiagnosticSignHint", text = "" },
-		{ name = "DiagnosticSignInfo", text = "" },
-	}
+    { name = "DiagnosticSignError", text = "" },
+    { name = "DiagnosticSignWarn",  text = "" },
+    { name = "DiagnosticSignHint",  text = "" },
+    { name = "DiagnosticSignInfo",  text = "" },
+  }
 
-	for _, sign in ipairs(signs) do
-		vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = "" })
-	end
+  for _, sign in ipairs(signs) do
+    vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = "" })
+  end
 
-	local config = {
-		virtual_text = false,
-		signs = {
-			active = signs, -- show signs
-		},
-		update_in_insert = true,
-		underline = true,
-		serverity_sort = true,
-		float = {
-			focusable = true,
-			style = "minimal",
-			border = "rounded",
-			source = "always",
-			header = "",
-			prefix = "",
-		},
-	}
+  local config = {
+    virtual_text = false,
+    signs = {
+      active = signs, -- show signs
+    },
+    update_in_insert = true,
+    underline = true,
+    serverity_sort = true,
+    float = {
+      focusable = true,
+      style = "minimal",
+      border = "rounded",
+      source = "always",
+      header = "",
+      prefix = "",
+    },
+  }
 
-	vim.diagnostic.config(config)
+  vim.diagnostic.config(config)
 
-	vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
-		border = "rounded",
-	})
+  vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
+    border = "rounded",
+  })
 
-	vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
-		border = "rounded",
-	})
+  vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
+    border = "rounded",
+  })
 end
 
 local function lsp_keymaps(bufnr)
-	local opts = { noremap = true, silent = true, buffer = bufnr }
-	local keymap = vim.keymap.set
-	keymap("n", "J", vim.diagnostic.open_float, opts)
-	keymap("n", "<leader>f", function()
-		vim.lsp.buf.format({ async = true })
-	end, opts)
-	keymap("n", "gd", vim.lsp.buf.definition, opts)
+  local opts = { noremap = true, silent = true, buffer = bufnr }
+  local keymap = vim.keymap.set
+  keymap("n", "J", vim.diagnostic.open_float, opts)
+  keymap("n", "<leader>f", function()
+    vim.lsp.buf.format({ async = true })
+  end, opts)
+  keymap("v", "<leader>f", function()
+    local start_row, _ = unpack(vim.api.nvim_buf_get_mark(0, "<"))
+    local end_row, _ = unpack(vim.api.nvim_buf_get_mark(0, ">"))
+    vim.lsp.buf.format({
+      range = {
+        ["start"] = { start_row, 0 },
+        ["end"] = { end_row, 0 },
+      },
+      async = true,
+    })
+  end, opts)
+  keymap("n", "gd", vim.lsp.buf.definition, opts)
 
-	-- Code action
-	keymap("n", "<leader>a", vim.lsp.buf.code_action, opts)
+  -- Code action
+  keymap("n", "<leader>a", vim.lsp.buf.code_action, opts)
 
-	-- Hover doc
-	keymap("n", "K", vim.lsp.buf.hover, opts)
-	-- Rename
-	keymap("n", "gr", vim.lsp.buf.rename, opts)
-	-- Preview definition
-	keymap("n", "gD", vim.lsp.buf.declaration, opts)
-	-- Jump to error
-	keymap("n", "]e", vim.diagnostic.goto_next, opts)
-	keymap("n", "[e", vim.diagnostic.goto_prev, opts)
+  -- Hover doc
+  keymap("n", "K", vim.lsp.buf.hover, opts)
+  -- Rename
+  keymap("n", "gr", vim.lsp.buf.rename, opts)
+  -- Preview definition
+  keymap("n", "gD", vim.lsp.buf.declaration, opts)
+  -- Jump to error
+  keymap("n", "]e", vim.diagnostic.goto_next, opts)
+  keymap("n", "[e", vim.diagnostic.goto_prev, opts)
 end
 
 M.on_attach = function(client, bufnr)
-	if client.name == "tsserver" then
-		client.server_capabilities.documentFormattingProvider = false
-	end
+  if client.name == "tsserver" then
+    client.server_capabilities.documentFormattingProvider = false
+  end
 
-	if client.name == "html" then
-		client.server_capabilities.documentFormattingProvider = false
-	end
+  if client.name == "html" then
+    client.server_capabilities.documentFormattingProvider = false
+  end
 
-	if client.name == "sumneko_lua" then
-		client.server_capabilities.documentFormattingProvider = false
-	end
+  if client.name == "sumneko_lua" then
+    client.server_capabilities.documentFormattingProvider = false
+  end
 
-	if client.name == "gopls" then
-		client.server_capabilities.documentFormattingProvider = false
-	end
+  if client.name == "gopls" then
+    client.server_capabilities.documentFormattingProvider = false
+  end
 
-	lsp_keymaps(bufnr)
+  lsp_keymaps(bufnr)
 end
 
 return M
--- .config/nvim/plugin/packer_compiled.lua
+++ .config/nvim/plugin/packer_compiled.lua
@@ -126,7 +126,7 @@ _G.packer_plugins = {
     url = "https://github.com/rafamadriz/friendly-snippets"
   },
   ["git-conflict.nvim"] = {
-    config = { "\27LJ\2\nm\0\0\4\0\6\0\t6\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\4\0005\3\3\0=\3\5\2B\0\2\1K\0\1\0\21default_mappings\1\0\0\1\0\2\tnext\a]x\tprev\a[x\nsetup\17git-conflict\frequire\0" },
+    config = { "\27LJ\2\nm\0\0\4\0\6\0\t6\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\4\0005\3\3\0=\3\5\2B\0\2\1K\0\1\0\21default_mappings\1\0\0\1\0\2\tprev\a[x\tnext\a]x\nsetup\17git-conflict\frequire\0" },
     loaded = true,
     path = "/Users/SangND/.local/share/nvim/site/pack/packer/start/git-conflict.nvim",
     url = "https://github.com/akinsho/git-conflict.nvim"
@@ -236,7 +236,7 @@ _G.packer_plugins = {
 time([[Defining packer_plugins]], false)
 -- Config for: git-conflict.nvim
 time([[Config for git-conflict.nvim]], true)
-try_loadstring("\27LJ\2\nm\0\0\4\0\6\0\t6\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\4\0005\3\3\0=\3\5\2B\0\2\1K\0\1\0\21default_mappings\1\0\0\1\0\2\tnext\a]x\tprev\a[x\nsetup\17git-conflict\frequire\0", "config", "git-conflict.nvim")
+try_loadstring("\27LJ\2\nm\0\0\4\0\6\0\t6\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\4\0005\3\3\0=\3\5\2B\0\2\1K\0\1\0\21default_mappings\1\0\0\1\0\2\tprev\a[x\tnext\a]x\nsetup\17git-conflict\frequire\0", "config", "git-conflict.nvim")
 time([[Config for git-conflict.nvim]], false)
 -- Config for: Navigator.nvim
 time([[Config for Navigator.nvim]], true)
