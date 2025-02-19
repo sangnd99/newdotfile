a93ed9d69d6429d6803eab5b6ca580cc55231dc1
sangnd <sang.nguyen@navigosgroup.com>
2025-02-19 13:19:12 +0700

    update config

--- .config/nvim/lua/plugins/auto-format.lua
+++ .config/nvim/lua/plugins/auto-format.lua
@@ -1,18 +1,20 @@
 return {
 	{
-		'nvimtools/none-ls.nvim',
+		"nvimtools/none-ls.nvim",
 		dependencies = {
-			'nvim-lua/plenary.nvim'
+			"nvim-lua/plenary.nvim",
 		},
 		config = function()
-			local null_ls = require('null-ls')
+			local null_ls = require("null-ls")
 			null_ls.setup({
-				null_ls.builtins.formatting.stylua,
-				null_ls.builtins.formatting.prettier,
-				null_ls.builtins.completion.spell
+				sources = {
+					null_ls.builtins.formatting.stylua,
+					null_ls.builtins.formatting.prettier,
+					null_ls.builtins.completion.spell,
+				},
 			})
 
-			vim.keymap.set('n', '<leader>f', '<cmd>lua vim.lsp.buf.format()<cr>', { desc = 'Formatting' })
-		end
-	}
+			vim.keymap.set({ "n", "v" }, "<leader>f", "<cmd>lua vim.lsp.buf.format()<cr>", { desc = "Formatting" })
+		end,
+	},
 }
