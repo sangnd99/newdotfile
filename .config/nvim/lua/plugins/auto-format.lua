0265dd4a82f54ee9bd7b3cab55148c7009eea3f7
Nguyễn Sang <sang.nguyen@spiritlabs.co>
2025-03-16 21:54:07 +0700

    update config

--- .config/nvim/lua/plugins/auto-format.lua
+++ .config/nvim/lua/plugins/auto-format.lua
@@ -14,12 +14,30 @@ return {
 			})
 
 			vim.keymap.set({ "n", "v" }, "<leader>f", function()
-				vim.lsp.buf.format({
-					filter = function(client)
-						return client.name == "null-ls" or client.name == "none-ls"
-					end,
-					async = true,
-				})
+				local mode = vim.api.nvim_get_mode().mode
+				if mode == "v" or mode == "V" or mode == "<C-v>" then
+					-- Visual mode: format only the selected range
+					local start_pos = vim.api.nvim_buf_get_mark(0, "<")
+					local end_pos = vim.api.nvim_buf_get_mark(0, ">")
+					vim.lsp.buf.format({
+						range = {
+							["start"] = { start_pos[1], start_pos[2] },
+							["end"] = { end_pos[1], end_pos[2] },
+						},
+						filter = function(client)
+							return client.name == "null-ls" or client.name == "none-ls"
+						end,
+						async = true,
+					})
+				else
+					-- Normal mode: format the whole file
+					vim.lsp.buf.format({
+						filter = function(client)
+							return client.name == "null-ls" or client.name == "none-ls"
+						end,
+						async = true,
+					})
+				end
 			end, { desc = "Formatting" })
 		end,
 	},
