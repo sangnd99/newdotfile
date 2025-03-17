bc24c87db0c72d5f862c04a00a5a5e945f09703c
Nguyễn Sang <sang.nguyen@spiritlabs.co>
2025-03-17 22:14:42 +0700

    update config

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -35,25 +35,17 @@ return {
 				pickers = {
 					-- Default configuration for builtin pickers goes here:
 					find_files = {
-						theme = "dropdown",
-						previewer = false,
-						layout_config = { height = 40 },
+						theme = "ivy",
+						layout_config = { height = 100 },
 					},
 					buffers = {
-						theme = "dropdown",
-						previewer = false,
-						layout_config = { height = 40 },
+						theme = "ivy",
+						layout_config = { height = 100 },
 					},
 					-- Now the picker_config_key will be applied every time you call this
 					-- builtin picker
 				},
-				extensions = {
-					file_browser = {
-						theme = "dropdown",
-						-- disables netrw and use telescope-file-browser in its place
-						hijack_netrw = true,
-					},
-				},
+				extensions = {},
 			})
 
 			telescope.load_extension("rest")
@@ -61,7 +53,13 @@ return {
 			vim.keymap.set("n", "<leader>pf", builtin.find_files, { desc = "Global find file" })
 			vim.keymap.set("n", "<leader>pb", builtin.buffers, { desc = "Opened buffer" })
 			vim.keymap.set("n", "<leader>ps", function()
-				builtin.grep_string({ search = vim.fn.input("Grep > ") })
+				local opts = require("telescope.themes").get_ivy({
+					layout_config = {
+						height = 100
+					},
+					search = vim.fn.input("Grep > "),
+				})
+				builtin.grep_string(opts)
 			end, { desc = "Grep search" })
 		end,
 	},
